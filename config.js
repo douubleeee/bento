db2335f93f42c177b61efae0628419125f5c29f8
douubleeee <hype8335@gmail.com>
2025-06-21 00:19:13 +0300

    config.js

--- config.js
+++ config.js
@@ -77,9 +77,9 @@ const CONFIG = {
 		},
 		{
 			id: '5',
-			name: 'Reddit',
+			name: 'Pinterest',
 			icon: 'glasses',
-			link: 'https://reddit.com',
+			link: 'https://ru.pinterest.com/',
 		},
 		{
 			id: '6',
